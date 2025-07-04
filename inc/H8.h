f7032a0b4478660ec7d8ce06ccd7f6d4e8e0981b
Gonzalo EA5JQP <ea5jqp@proton.me>
2025-07-04 10:42:43 +0200

    pinout for h8

---
+++ inc/H8.h
@@ -0,0 +1,54 @@
+// H8_Pinmap.h — Pin Definitions for H8 Board
+
+#define APC           P02     // Pin 1  -> APC
+#define LCDRST        P03     // Pin 2  -> LCDRST
+#define LCDSCLK       P04     // Pin 3  -> LCDSCLK
+#define LCDSDA        P05     // Pin 4  -> LCDSDA
+#define LCDCS         P06     // Pin 5  -> LCDCS
+#define LCDAO         P07     // Pin 6  -> LCDAO
+
+// Pin 7 is GND
+
+#define CODE2         P50     // Pin 8  -> CODE2
+#define LAMPOW        P51     // Pin 9  -> LAMPOW
+#define CODE1         P52     // Pin 10 -> CODE1
+#define POW2822       P53     // Pin 11 -> POW2822
+#define AUDIO_SDA     P54     // Pin 12 -> AUDIO_SDA
+#define AUDIO_SCK     P55     // Pin 13 -> AUDIO_SCK
+#define BT_ONOFF      P47     // Pin 14 -> BT_ONOFF
+#define BEEP          P46     // Pin 15 -> BEEP
+
+// Pin 16 is 3V3
+
+#define SDA24         P45     // Pin 17 -> SDA24
+#define SCK24         P44     // Pin 18 -> SCK24
+#define BACKLED       P43     // Pin 19 -> BACKLED
+#define VRX_P         P42     // Pin 20 -> VRX-P
+#define URX_P         P41     // Pin 21 -> URX-P
+#define BATT_AD       P40     // Pin 22 -> BATT-AD
+#define BT_LED        P37     // Pin 23 -> BT_LED
+#define VTX_P         P36     // Pin 24 -> VTX-P
+#define UTX_P         P35     // Pin 25 -> UTX-P
+#define KEYIN1        P34     // Pin 26 -> KEYIN1
+#define KEYIN2        P33     // Pin 27 -> KEYIN2
+#define KEYIN3        P32     // Pin 28 -> KEYIN3
+#define KEYIN4        P31     // Pin 29 -> KEYIN4
+#define KEYIN5        P30     // Pin 30 -> KEYIN5
+#define TXLED         P10     // Pin 31 -> TXLED
+#define RXD232_PTT    P11     // Pin 32 -> RXD232/PTT
+#define RXLED         P12     // Pin 33 -> RXLED
+#define TXD232        P13     // Pin 34 -> TXD232
+#define KEYOUT4       P14     // Pin 35 -> KEYOUT4
+#define KEYOUT3       P15     // Pin 36 -> KEYOUT3
+#define KEYOUT2       P16     // Pin 37 -> KEYOUT2
+#define KEYOUT1       P17     // Pin 38 -> KEYOUT1
+#define BT_RXD        P20     // Pin 39 -> BT_RXD
+#define BT_TXD        P21     // Pin 40 -> BT_TXD
+#define SDA1080       P22     // Pin 41 -> SDA1080
+#define SCK1080       P23     // Pin 42 -> SCK1080
+#define LE1846        P24     // Pin 43 -> LE1846
+#define SDA1846       P25     // Pin 44 -> SDA1846
+#define SCK1846       P26     // Pin 45 -> SCK1846
+#define DECODE        P27     // Pin 46 -> DECODE
+#define SQL_AD        P00     // Pin 47 -> SQL-AD
+#define VOX_AD        P01     // Pin 48 -> VOX-AD
